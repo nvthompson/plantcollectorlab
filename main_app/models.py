ee9ab40c6389f557a4e8c169d2cf6f8ac61ca4d4
Nicole Thompson <Nicole@MacBook-Pro-2.local>
2022-02-18 08:59:36 -0800

    created a detail page and added a superuser

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Plant(models.Model):
+    name = models.CharField(max_length=50)
+    type = models.CharField(max_length=50)
+    color = models.CharField(max_length=50)
+    description = models.CharField(max_length=250)
+    
+    def __str__(self):
+        return self.name
--- main_app/urls.py
+++ main_app/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
     path('plants/', views.plants_index, name='index'),
-    # path('plants/<int:plant_id>/', views.plants_detail, name='detail'),
+    path('plants/<int:plant_id>/', views.plants_detail, name='detail'),
     # path('plants/create/', views.PlantCreate.as_view(), name='plants_create'),
 ]
